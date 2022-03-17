94dc184080cca5a98442b48f40045bd25a9d25f9
orahj <isaackells1026@gmail.com>
2022-03-17 05:29:48 +0100

    Initial commit

---
+++ Persistence/Migrations/20220317033752_UpdateActivity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class UpdateActivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Activities",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "description");
+        }
+    }
+}
